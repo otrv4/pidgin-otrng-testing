24f172015857626d339d470413fbafd67db885be
Giovane Liberato <giovane@autonomia.digital>
2018-10-10 18:31:44 -0300

    Increment test case

--- test_interactive_path.py
+++ test_interactive_path.py
@@ -7,7 +7,7 @@ from dogtail.config import config
 from dogtail.rawinput import pressKey, typeText
 from dogtail.predicate import GenericPredicate
 from dogtail.procedural import click, focus, select, type
-from dogtail.tree import root
+from dogtail.tree import root, SearchError
 from dogtail.utils import run
 
 def start_pidgin():
@@ -26,10 +26,13 @@ def start_private_conversation():
     click.menu('OTR')
     click.menuItem("Start private conversation")
 
+def end_private_conversation():
+    click.menu('OTR')
+    click.menuItem("End private conversation")
+
 def send_message(buddy, message):
     focus.window(buddy)
     focus.text()
-    time.sleep(1)
     type(message)
     pressKey("Enter")
 
@@ -37,14 +40,18 @@ def assert_otr_status(expected):
     try:
         # TODO remove this space from pidgin UI, probably used for formating
         focus.window.button(' ' + expected)
-    except:
+    except SearchError:
         assert False
 
-def assert_message_was_sent_by(sender, message):
+def assert_message_is_present(sender, message):
     chat_tab = focus.window.findChild(GenericPredicate(name=sender, roleName='page tab'))
     chat_box = chat_tab.findChild(GenericPredicate(roleName='text'))
     message_len = len(message)
     assert message == chat_box.text[-message_len:]
+
+def switch_conversation_tab_with(tab_name):
+    chat_tab = focus.window.findChild(GenericPredicate(name=tab_name, roleName='page tab'))
+    chat_tab.click()
 
 if __name__ == '__main__':
     config.searchCutoffCount = 3
@@ -54,12 +61,24 @@ if __name__ == '__main__':
 
     pid = start_pidgin()
     root.application('Pidgin')
+
     open_conversation_with("bob@localhost")
     assert_otr_status('Not private')
     start_private_conversation()
     assert_otr_status('Unverified')
+    assert_message_is_present('bob@localhost' ,'Unverified conversation started.  Your client is not logging this conversation.')
     send_message('bob@localhost', 'Hi bob!')
-    assert_message_was_sent_by('bob@localhost', 'Hi bob!')
+    assert_message_is_present('bob@localhost', 'Hi bob!')
+
+    switch_conversation_tab_with('alice@localhost')
+    assert_otr_status('Unverified')
+    #assert_message_is_present('bob@localhost' ,'Unverified conversation started.  Your client is not logging this conversation.')
+    assert_message_is_present('alice@localhost', 'Hi bob!')
+
+    switch_conversation_tab_with('bob@localhost')
+    end_private_conversation()
+    assert_message_is_present('bob@localhost', 'Private conversation lost.')
+    assert_otr_status('Not private')
 
     raw_input("End?\n")
     pid.terminate()
